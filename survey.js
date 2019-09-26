74feb52d137ce0ee036bc463ee4bafe958d01aaf
Keyvan <k.kholghi@gmail.com>
2019-09-26 17:19:15 +0000

    Revised by adding destructuring array to my survey.js

--- survey.js
+++ survey.js
@@ -5,15 +5,24 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
-rl.question("What's your name? Nicknames are also acceptable :)", (myName) => {
-    rl.question("What's an activity you like doing?", (favoriteActivity) => {
-        rl.question("What do you listen to while doing that?", (favoriteMusic) => {
-            rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)", (favoriteMeal) => {
-                rl.question("What\'s your favourite thing to eat for that meal?", (favoriteSides) => {
-                    rl.question("Which sport is your absolute favourite?", (favoriteSport) => {
-                        rl.question("What is your superpower? In a few words, tell us what you are amazing at!", (amazingAt) => {
+const answers = [];
+rl.question("What's your name? Nicknames are also acceptable :)", (answer) => {
+    answers.push(answer);
+    rl.question("What's an activity you like doing?", (answer) => {
+        answers.push(answer);
+        rl.question("What do you listen to while doing that?", (answer) => {
+            answers.push(answer);
+            rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)", (answer) => {
+                answers.push(answer);
+                rl.question("What\'s your favourite thing to eat for that meal?", (answer) => {
+                    answers.push(answer);
+                    rl.question("Which sport is your absolute favourite?", (answer) => {
+                        answers.push(answer);
+                        rl.question("What is your superpower? In a few words, tell us what you are amazing at!", (answer) => {
+                            answers.push(answer)
+                            const [myName, favoriteActivity, favoriteMusic, favoriteMeal, favoriteSides, favoriteSport, amazingAt] = answers
                             let print = `My name is ${myName}. I would like ${favoriteActivity}, listening to ${favoriteMusic}, and eating ${favoriteMeal} with ${favoriteSides}. Furthermore, I'm fan of ${favoriteSport} and literally superpower at ${amazingAt}`;
+                            console.log();
                             console.log(print);
                             rl.close();
                         });
